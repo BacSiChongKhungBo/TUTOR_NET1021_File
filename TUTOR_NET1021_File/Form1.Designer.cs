d4564b944bb39abec4bb8c02d7db592670d7edc9
BacSiChongKhungBo <huywewe123@gmail.com>
2024-04-04 13:58:14 +0700

    Add project files.

---
+++ TUTOR_NET1021_File/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TUTOR_NET1021_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_Path = new TextBox();
+            rtxt_NoiDung = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btn_Doc = new Button();
+            btn_Ghi = new Button();
+            txt_MaSV = new TextBox();
+            txt_HoTen = new TextBox();
+            txt_Tuoi = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            btn_GhiSV = new Button();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // txt_Path
+            // 
+            txt_Path.Location = new Point(12, 96);
+            txt_Path.Name = "txt_Path";
+            txt_Path.Size = new Size(219, 23);
+            txt_Path.TabIndex = 0;
+            // 
+            // rtxt_NoiDung
+            // 
+            rtxt_NoiDung.Location = new Point(12, 213);
+            rtxt_NoiDung.Name = "rtxt_NoiDung";
+            rtxt_NoiDung.Size = new Size(364, 225);
+            rtxt_NoiDung.TabIndex = 1;
+            rtxt_NoiDung.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(12, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(216, 37);
+            label1.TabIndex = 2;
+            label1.Text = "Địa chỉ file (Path)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(12, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 37);
+            label2.TabIndex = 3;
+            label2.Text = "Nội Dung";
+            // 
+            // btn_Doc
+            // 
+            btn_Doc.Font = new Font("Segoe UI", 20F);
+            btn_Doc.Location = new Point(247, 67);
+            btn_Doc.Name = "btn_Doc";
+            btn_Doc.Size = new Size(129, 64);
+            btn_Doc.TabIndex = 4;
+            btn_Doc.Text = "Đọc";
+            btn_Doc.UseVisualStyleBackColor = true;
+            btn_Doc.Click += btn_Doc_Click;
+            // 
+            // btn_Ghi
+            // 
+            btn_Ghi.Font = new Font("Segoe UI", 20F);
+            btn_Ghi.Location = new Point(247, 143);
+            btn_Ghi.Name = "btn_Ghi";
+            btn_Ghi.Size = new Size(129, 64);
+            btn_Ghi.TabIndex = 5;
+            btn_Ghi.Text = "Ghi";
+            btn_Ghi.UseVisualStyleBackColor = true;
+            btn_Ghi.Click += btn_Ghi_Click;
+            // 
+            // txt_MaSV
+            // 
+            txt_MaSV.Location = new Point(533, 96);
+            txt_MaSV.Name = "txt_MaSV";
+            txt_MaSV.Size = new Size(219, 23);
+            txt_MaSV.TabIndex = 6;
+            // 
+            // txt_HoTen
+            // 
+            txt_HoTen.Location = new Point(533, 143);
+            txt_HoTen.Name = "txt_HoTen";
+            txt_HoTen.Size = new Size(219, 23);
+            txt_HoTen.TabIndex = 7;
+            // 
+            // txt_Tuoi
+            // 
+            txt_Tuoi.Location = new Point(533, 187);
+            txt_Tuoi.Name = "txt_Tuoi";
+            txt_Tuoi.Size = new Size(219, 23);
+            txt_Tuoi.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 20F);
+            label3.Location = new Point(382, 82);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 37);
+            label3.TabIndex = 9;
+            label3.Text = "Mã SV";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 20F);
+            label4.Location = new Point(382, 128);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 37);
+            label4.TabIndex = 10;
+            label4.Text = "Họ Tên";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 20F);
+            label5.Location = new Point(382, 173);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 37);
+            label5.TabIndex = 11;
+            label5.Text = "Tuổi";
+            // 
+            // btn_GhiSV
+            // 
+            btn_GhiSV.Font = new Font("Segoe UI", 20F);
+            btn_GhiSV.Location = new Point(623, 236);
+            btn_GhiSV.Name = "btn_GhiSV";
+            btn_GhiSV.Size = new Size(129, 64);
+            btn_GhiSV.TabIndex = 12;
+            btn_GhiSV.Text = "Ghi";
+            btn_GhiSV.UseVisualStyleBackColor = true;
+            btn_GhiSV.Click += btn_GhiSV_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20F);
+            button1.Location = new Point(414, 236);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 64);
+            button1.TabIndex = 13;
+            button1.Text = "Đọc";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(382, 315);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(186, 23);
+            comboBox1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 20F);
+            button2.Location = new Point(585, 315);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 51);
+            button2.TabIndex = 15;
+            button2.Text = "Load";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 621);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(btn_GhiSV);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txt_Tuoi);
+            Controls.Add(txt_HoTen);
+            Controls.Add(txt_MaSV);
+            Controls.Add(btn_Ghi);
+            Controls.Add(btn_Doc);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(rtxt_NoiDung);
+            Controls.Add(txt_Path);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt_Path;
+        private RichTextBox rtxt_NoiDung;
+        private Label label1;
+        private Label label2;
+        private Button btn_Doc;
+        private Button btn_Ghi;
+        private TextBox txt_MaSV;
+        private TextBox txt_HoTen;
+        private TextBox txt_Tuoi;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button btn_GhiSV;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Button button2;
+    }
+}
---
+++ TUTOR_NET1021_File/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+
+namespace TUTOR_NET1021_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Một số lưu ý:
+            // 1.
+            // Path mặc định: E:\NET1021.txt
+            // Path trong chương trình: E:\\NET1021.txt
+            // => nếu trong path xuất hiện dấu \ => đổi thành \\
+            // 2. 
+            // Nếu không close File | reader | write => có thể lỗi do xung đột luồng
+            // 3.
+            // Khi đóng luồng , đóng Reader và Writer trước
+            // dóng Filestream cuối cùng
+        }
+
+        private void btn_Doc_Click(object sender, EventArgs e)
+        {
+            //4 bước cho đọc file
+            //B1: FileStream : yêu cầu khoảng 2 đầu vào
+            // - Path : địa chỉ file
+            // - FileMode: file sẽ có thể làm gì
+            //C1:
+            //FileStream fs = new FileStream(txt_Path.Text, FileMode.OpenOrCreate);
+            //C2:
+            string path = "E:\\NET1021.txt";
+            FileMode mode = FileMode.OpenOrCreate;
+            FileStream fs = new FileStream(path, mode);
+            //B2: StreamReader: bộ đọc dữ liệu
+            StreamReader sr = new StreamReader(fs); // lấy dữ liệu ra để đọc từ FileStream
+            //B3: tiến hành đọc và gán kết quả
+            string result = sr.ReadToEnd(); // đọc đến hết luồng file
+            rtxt_NoiDung.Text = result;
+            //B4: Tiến hành thông báo và đóng các luồng
+            sr.Close();
+            fs.Close();
+            MessageBox.Show("Đọc file thành công");
+        }
+
+        private void btn_Ghi_Click(object sender, EventArgs e)
+        {
+            //File Stream
+            FileStream fs = new FileStream(txt_Path.Text, FileMode.OpenOrCreate);
+            //StreamWriter
+            StreamWriter sw = new StreamWriter(fs, Encoding.Unicode);
+            //dùng StreamWriter để ghi vào file
+            sw.Write(rtxt_NoiDung.Text);
+            //Đóng các Stream lại
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("Ghi File thành công");
+        }
+
+        private void btn_GhiSV_Click(object sender, EventArgs e)
+        {
+            //File Stream
+            string path = "E:\\NET1021_SV.txt";
+            //FileMode.Append: tạo hoặc tìm đến dòng cuối cùng của file
+            FileStream fs = new FileStream(path, FileMode.Append);
+            //StreamWriter
+            StreamWriter sw = new StreamWriter(fs, Encoding.Unicode);
+            string content = $"{txt_MaSV.Text} | {txt_HoTen.Text} | {txt_Tuoi.Text}\n";
+            sw.Write(content);
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("Ghi File thành công");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "E:\\NET1021_SV.txt";
+            FileMode mode = FileMode.OpenOrCreate;
+            FileStream fs = new FileStream(path, mode);
+            StreamReader sr = new StreamReader(fs);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            fs.Close();
+            MessageBox.Show("Đọc file thành công");
+            string[] data = GetData(result);
+            txt_MaSV.Text = data[0];
+            txt_HoTen.Text = data[1];
+            txt_Tuoi.Text = data[2];
+        }
+
+        //Tạo nút đọc File, điền ngược lại những gì đã lưu vào file vào textbox
+        //gợi ý : sử dụng split
+
+        public string[] GetData(string reader)
+        {
+            string[] arr = reader.Split(" | ");
+            return arr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = "E:\\quan.txt";
+            FileMode mode = FileMode.OpenOrCreate;
+            FileStream fs = new FileStream(path, mode);
+            StreamReader sr = new StreamReader(fs);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            fs.Close();
+            MessageBox.Show("Đọc file thành công");
+            string[] data = GetQuan(result);
+            foreach(var item in data)
+            {
+                comboBox1.Items.Add(item);
+            }
+            comboBox1.SelectedIndex = 0;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        public string[] GetQuan(string reader)
+        {
+            string[] arr = reader.Split("\n");
+            return arr;
+        }
+    }
+}
